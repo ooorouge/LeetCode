a1d3d0555162868108ae7599b9598c1c9b1d199a
ooorouge <37837318+ooorouge@users.noreply.github.com>
2018-12-01 15:31:42 +0800

    Add files via upload

---
+++ No.3 Substring/no3_substring.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+#最开始感觉用stack来做，应该可行，估计是有些地方写错了，有时间再改改
+'''
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d, res, start = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in d: start = max(start, d[ch]+1)
+            res = max(res, i-start+1)                
+            d[ch] = i
+        return res
+'''
+        d, res, start = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in d: start = max(start, d[ch]+1)
+            res = max(res, i-start+1)                
+            d[ch] = i
+        return res
+        
